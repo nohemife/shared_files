aa4b690891d5b40010344d23964a4bb75228a490
nohemife <152927654+nohemife@users.noreply.github.com>
2025-06-22 17:56:45 +0300

    Update esp32.c

--- esp32.c
+++ esp32.c
@@ -1,1 +1,11 @@
 https://gist.github.com/bvernoux/6ddc02ed157417a4df37861488357c5a
+
+https://forum.cxem.net/index.php?/topic/270821-fatfs-stm32f103-spi-w25q32-%D0%BD%D0%B5-%D0%BD%D0%B0%D1%85%D0%BE%D0%B4%D0%B8%D1%82-%D1%84%D0%B0%D0%B9%D0%BB%D1%8B-%D0%BA%D0%BE%D1%82%D0%BE%D1%80%D1%8B%D0%B5-%D0%B5%D1%81%D1%82%D1%8C/
+https://habr.com/ru/articles/730232/
+
+https://habr.com/ru/articles/441660/
+https://habr.com/ru/articles/441858/
+
+https://eax.me/stm32-fatfs/
+https://programmersought.com/article/43424360320/
+https://electronix.ru/forum/topic/133713-stm32f373-flashat25df081-fatfs/
